0fedebd72d41b8048e508bf55e010c4239aa5af6
Pudgey-podge <68105627+Pudgey-podge@users.noreply.github.com>
2024-12-15 13:01:50 +0000

    Add files via upload

---
+++ run.py
@@ -0,0 +1,84 @@
+"""Run moving averages strategy on Uniswap."""
+from decimal import Decimal
+from typing import Any, Optional
+
+from policy import MovingAveragePolicy
+
+from dojo.agents.uniswapV3 import TotalWealthAgent
+from dojo.common import time_to_block
+from dojo.common.constants import Chain
+from dojo.environments import UniswapV3Env
+from dojo.runners import backtest_run
+
+
+def main(
+    *,
+    dashboard_server_port: Optional[int],
+    simulation_status_bar: bool,
+    auto_close: bool,
+    num_sim_blocks: int = 3000,
+    **kwargs: dict[str, Any],
+) -> None:
+    """Running this strategy."""
+    pools = ["USDC/WETH-0.05"]
+    start_time = "2021-06-28 00:00:00"
+    chain = Chain.ETHEREUM
+
+    # Agents
+    mavg_agent = TotalWealthAgent(
+        initial_portfolio={
+            "ETH": Decimal(100),
+            "USDC": Decimal(10_000),
+            "WETH": Decimal(1),
+        },
+        name="MAvg_Agent",
+        unit_token="USDC",
+    )
+    lp_agent = TotalWealthAgent(
+        initial_portfolio={"USDC": Decimal(10_000), "WETH": Decimal(1)},
+        name="LP_Agent",
+        unit_token="USDC",
+    )
+
+    # Simulation environment (Uniswap V3)
+    env = UniswapV3Env(
+        chain=Chain.ETHEREUM,
+        block_range=(
+            time_to_block(start_time, chain),
+            time_to_block(start_time, chain) + num_sim_blocks,
+        ),
+        agents=[mavg_agent, lp_agent],
+        pools=pools,
+        backend_type="forked",  # change to local for better speed
+        market_impact="replay",
+    )
+
+    # Policies
+    mavg_policy = MovingAveragePolicy(
+        agent=mavg_agent, pool="USDC/WETH-0.05", short_window=25, long_window=100
+    )
+
+    # SNIPPET 1 START
+    backtest_run(
+        env=env,
+        policies=[mavg_policy],
+        dashboard_server_port=dashboard_server_port,
+        output_file="moving_averages.db",
+        auto_close=auto_close,
+        simulation_status_bar=simulation_status_bar,
+        simulation_title="Moving averages",
+        simulation_description="Moving Average Strategy	Also known as mean reversion or mean crossover strategy.",
+    )
+    # SNIPPET 1 END
+
+
+if __name__ == "__main__":
+    import dojo.config.logging_config
+
+    dojo.config.logging_config.set_normal_logging_config_and_print_explanation()
+    main(
+        dashboard_server_port=8768,
+        simulation_status_bar=True,
+        auto_close=False,
+        num_sim_blocks=300,
+    )
